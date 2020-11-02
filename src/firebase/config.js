7cb0d15ed12c0af0621afa4dcd8421143c55ba3c
Vinh Luong <vinhluongtvivatva@gmail.com>
2020-11-03 02:57:13 +1100

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,14 @@
 import Firebase from "firebase";
+require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3JZhsdlCngeq2AK3wpOzewO0Hy7Gr8tI",
-  authDomain: "react-messenger-app-c30ce.firebaseapp.com",
-  databaseURL: "https://react-messenger-app-c30ce.firebaseio.com",
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_DATABASEURL,
   projectId: "react-messenger-app-c30ce",
   storageBucket: "react-messenger-app-c30ce.appspot.com",
   messagingSenderId: "700435432402",
-  appId: "1:700435432402:web:3ba6a86653ab08e4a33e0b",
+  appId: process.env.REACT_APP_ID,
   measurementId: "G-EE8L4TBQM2",
 };
 // Initialize Firebase
